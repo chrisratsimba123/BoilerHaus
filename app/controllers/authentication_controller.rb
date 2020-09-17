259f86be51b336e0f757741f630100afdc4d217c
Chris Ratsimbazafy <ratsimbazafy@Chriss-MacBook-Air.local>
2020-09-17 16:32:24 -0400

    initialized BE and FE

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,28 @@
+class AuthenticationController < ApplicationController
+    before_action :authorize_request, except: :login
+
+    # POST /auth/login
+    def login
+        @artist = Artist.find_by(username: login_params[:username])
+        if @artist.authenticate(login_params[:password]) #authenticate method provided by Bcrypt and 'has_secure_password'
+            @token = encode({ id: @artist.id })
+            render json: {
+                artist: @artist.attributes.except('password_digest'),
+                token: @token
+            }, status: :ok
+        else 
+            render json: { errors: 'unauthorized' }, status: :unauthorized
+        end
+    end
+
+    #GET /auth/verify
+    def verify
+        render json: @current_artist.attributes.except('password_digest'), status: :ok
+    end
+
+    private
+
+    def login_params
+        params.require(:authentication).permit(:username, :password, :email)
+    end
+end
