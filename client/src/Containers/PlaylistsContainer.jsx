5c72bfc28a6d567164593741be8f2e00c5eadd97
Chris Ratsimbazafy <ratsimbazafy@Chriss-MacBook-Air.local>
2020-09-21 10:55:07 -0400

    created Playlists Container

--- client/src/Containers/PlaylistsContainer.jsx
+++ client/src/Containers/PlaylistsContainer.jsx
@@ -6,3 +6,65 @@ import Playlists from '../Screens/Playlists/Playlists'
 import PlaylistCreate from '../Screens/PlaylistCreate/PlaylistCreate'
 import PlaylistDetail from '../Screens/PlaylistDetail/PlaylistDetail'
 import PlaylistEdit from '../Screens/PlaylistEdit/PlaylistEdit'
+
+export default function PlaylistsContainer(props) {
+    const [playlists, setPlaylists] = useState([])
+    const history = useHistory()
+    const { currentUser } = props 
+
+    useEffect(() => {
+        const fetchPlaylists = async () => {
+            const playlistArray = await getAllPlaylists()
+            setPlaylists(playlistArray)
+        }
+        fetchPlaylists()
+    }, [])
+
+    const newPlaylist = async (formData) => {
+        const newlyCreatedPlaylist = await createPlaylist(id, formData)
+        setPlaylists(prevState => [...prevState, newlyCreatedPlaylist])
+        history.push('/playlists')
+    }
+
+    const editPlaylist = async (id, formData) => {
+        const updatedPlaylist = await updatePlaylist(id, formData)
+        setPlaylists(prevState => prevState.map(playlist => {
+            return playlist.id === Number(id) ? updatedPlaylist : playlist
+        }))
+        history.push(`/playlists/${id}`)
+    }
+
+    const deletePlaylist = async (id) => {
+        await destroyPlaylist(id)
+        setPlaylists(prevState => prevState.filter(playlist => {
+            return playlist.id !== id 
+        }))
+        history.push('/playlists')
+    }
+
+    return (
+        <Switch>
+            <Route path='/playlists/new'>
+                <PlaylistCreate 
+                    newPlaylist={newPlaylist}
+                />
+            </Route>
+            <Route path='/playlists/:id'>
+                <PlaylistDetail 
+                    currentUser={currentUser}
+                    deletePlaylist={deletePlaylist}
+                />
+            </Route>
+            <Route path='/playlists/:id/edit'>
+                <PlaylistEdit 
+                    editPlaylist={editPlaylist}
+                />
+            </Route>
+            <Route path='/playlists'>
+                <Playlists 
+                    playlists={playlists}
+                />
+            </Route>
+        </Switch>
+    )
+}
