b93e077da59798e9061c97ed1daf6c80f2a0bd37
Chris Ratsimbazafy <ratsimbazafy@Chriss-MacBook-Air.local>
2020-09-20 22:31:55 -0400

    created Artist prototype

---
+++ client/src/Components/Artist/Artist.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Artist = (props) => {
+    return (
+        <>
+            <Link className='artist' to={`/artists/${artist.id}`}>
+                <img className='artist-image' src={props.image} alt={props.username}/>
+                <div className='artist-name'>{props.username}</div>
+            </Link>
+        </>
+    )
+}
+
+export default Artist
