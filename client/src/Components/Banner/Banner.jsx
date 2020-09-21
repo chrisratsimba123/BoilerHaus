19310d13ca883f9afa923f8361c79dffddb9aa99
Chris Ratsimbazafy <ratsimbazafy@Chriss-MacBook-Air.local>
2020-09-21 11:38:36 -0400

    created Banner

---
+++ client/src/Components/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Banner(props) {
+
+    const { src, alt } = props
+
+    return (
+        <div>
+            <div className='banner'>
+                <img className='banner-image'
+                    src
+                />
+            </div>
+        </div>
+    )
+
+}
